a153def34e31c68f6ad31dbc817c8978f7258f08
Pablo de Jesús <pag753@gmail.com>
2017-12-13 10:04:15 -0600

    COMMIT DEL 13 DE DICIEMBRE DE 2017

--- SistemaExpertoPoligonos/FactFactory.cs
+++ SistemaExpertoPoligonos/FactFactory.cs
@@ -24,6 +24,38 @@ namespace SistemaExpertoPoligonos
                 newFact = new IntFact(f.Name(), value, null, 0);
             }
             return newFact;
+        }
+        
+        internal static IFact Fact(string factStr)
+        {
+            factStr = factStr.Trim();
+            if(factStr.Contains("="))
+            {
+                // Existe un símbolo '=' por lo que es un IntFactor,
+                // separamos el nombre del valor
+                String[] nameValue=factStr.Split(new String[] { "=", "(", ")"}, StringSplitOptions.RemoveEmptyEntries);
+                if(nameValue.Length>=2)
+                {
+                    String question = null;
+                    if(nameValue.Length==3)
+                    {
+                        // Podemos pedir, y recuperar,
+                        // la pregunta vinculada
+                        question = nameValue[2].Trim();
+                    }
+                    return new IntFact(nameValue[0].Trim(), int.Parse(nameValue[1].Trim()), question);
+                }
+                else
+                {
+                    // Sintaxis incorrecta
+                    return null;
+                }
+            }
+            else
+            {
+                // No hay símbolo igual, es un hecho de la clase BoolFact
+                bool value = true;
+            }
         }               
     }
 }
